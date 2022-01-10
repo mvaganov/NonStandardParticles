1ba374a4591ed165eb61ce62f66609b739c4ece5
Michael Vaganov <mvaganov@hotmail.com>
2022-01-10 00:48:17 -0800

    updated package.json

--- Scripts/NonStandard/Particles/ParticleCreator.cs
+++ Scripts/NonStandard/Particles/ParticleCreator.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleCreator : MonoBehaviour
-{
-    public ParticleSystem ps;
-    public bool createOnStart;
-    void Start() {
-        if (createOnStart) {
-            SpawnTemporary();
-        }
-    }
-    public void SpawnTemporary() {
-        GameObject go = Instantiate(ps.gameObject);
-        Transform goT = go.transform;
-        Transform t = transform;
-        goT.position = t.position;
-        goT.rotation = t.rotation;
-        Destroy(go, ps.main.duration);
-    }
+public class ParticleCreator : MonoBehaviour {
+	public ParticleSystem ps;
+	public bool createOnStart;
+	void Start() {
+		if (createOnStart) {
+			SpawnTemporary();
+		}
+	}
+	public void SpawnTemporary() {
+		GameObject go = Instantiate(ps.gameObject);
+		Transform goT = go.transform;
+		Transform t = transform;
+		goT.position = t.position;
+		goT.rotation = t.rotation;
+		Destroy(go, ps.main.duration);
+	}
 }
